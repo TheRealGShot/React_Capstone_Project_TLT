80289179bda6bad4d16186c99291f6f9cb98ecf4
Gabriel Sgarlato <ga.sgarlato.10@gmail.com>
2025-12-17 16:12:01 -0500

    done

--- Capstone1/src/components/Navbar.jsx
+++ Capstone1/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
-
 import styles from "../styles/Navbar.module.css";
+import { BookSearch } from 'lucide-react';
+import { CircleUserRound } from 'lucide-react';
+import logo from '../assets/logo.png'
 function Navbar(){
     return(
         <nav className={styles.nav}>
 
-            <div>
-                <img/>
-                <p>The Learning Tree</p>
+            <div className={styles.div1}>
+                <img className={styles.logo} src={logo}/>
+                <p className={styles.p}>The Learning Tree</p>
             </div>
-            <button>
-                <></>
+            <button className={styles.button}>
+                <BookSearch color="#000000" size={55}/>
             </button>
-            <div>
-                <p>Guest</p>
-                <></>
+            <div className={styles.div2}>
+                <p className={styles.p}>Guest</p>
+                <CircleUserRound color="#000000" size={65}/>
             </div>
         </nav>
     );
