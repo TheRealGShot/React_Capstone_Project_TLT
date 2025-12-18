7b51376ffdeef7d0237f9c6562ae575049053007
Gabriel Sgarlato <ga.sgarlato.10@gmail.com>
2025-12-18 09:45:10 -0500

    save

--- Capstone1/src/App.jsx
+++ Capstone1/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="div">
+        <Routes>
+          <Route path='' element={<Home/>}></Route>
+        </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ Capstone1/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+function Home(){
+    return(
+        <div>
+            <div>
+                <h1>Welcome To The Tree</h1>
+                <button></button>
+            </div>
+            <div>
+                <img></img>
+                <h2>The Learning Tree Library</h2>
+                <p>Over 100 Books to Borrow From! The library offers a wide collection of books that users can explore and learn from, covering different topics and interests. Visitors can easily find detailed information about each book and check whether it is available or currently borrowed. With an organized system and a welcoming atmosphere, the library makes discovering and enjoying books simple and exciting. </p>
+            </div>
+        </div>
+    );
+}
+export default Home;
