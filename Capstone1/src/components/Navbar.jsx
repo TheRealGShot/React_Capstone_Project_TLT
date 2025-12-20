21c66093fe96140785c466b3f37f80954a6c5107
TheRealGShot <ga.sgarlato.10@gmail.com>
2025-12-20 20:51:18 +0000

    updated

--- Capstone1/src/components/Navbar.jsx
+++ Capstone1/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { BookSearch } from 'lucide-react';
 import { CircleUserRound } from 'lucide-react';
 import logo from '../assets/logo.png'
 function Navbar(){
+    const [open, setOpen] = useState(false);
     return(
         <nav className={styles.nav}>
 
@@ -10,9 +11,10 @@ function Navbar(){
                 <img className={styles.logo} src={logo}/>
                 <p className={styles.p}>The Learning Tree</p>
             </div>
-            <button className={styles.button}>
-                <BookSearch color="#000000" size={55}/>
+            <button className={styles.button} onClick={()=> setOpen(!open)}>
+                {!open && <BookSearch color="#000000" size={55}/>}
             </button>
+
             <div className={styles.div2}>
                 <p className={styles.p}>Guest User</p>
                 <CircleUserRound color="#000000" size={65}/>
